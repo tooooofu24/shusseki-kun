7009ccbe29adc01209ae679ec76287dce8cce6a6
tooooofu24 <toya24xxx@i.softbank.jp>
2022-11-22 11:12:21 +0900

    headタグの調整

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,21 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <title>出席くん</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+        />
+        <meta name="description" content="学校の出席管理アプリ" />
+        <meta property="og:url" content="https://app.shusseki-kun.com" />
+        <meta property="og:title" content="出席くん" />
+        <meta property="og:description" content="学校の出席管理アプリ" />
+        <meta
+          property="og:image"
+          content="https://app.shusseki-kun.com/img/logo.png"
+        />
+        <meta property="og:site_name" content="出席くん" />
+        <meta property="og:locale" content="ja_JP" />
       </Head>
       <body>
         <Main />
